b2351a8b6d1cac4d249ef9b9934688570a86d411
John Wu <vidadelaempresa@gmail.com>
2014-09-02 19:58:45 +0100

    Add user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,14 @@ class User < ActiveRecord::Base
   
   validates :steam_name, uniqueness: true
 
-  before_save :set_steam_id
+  before_save :set_steam_data
 
-  def set_steam_id
-    if steam_id.blank?
+  def set_steam_data
+    if steam_id.blank? || avatar.blank?
       response = HTTParty.get("http://steamcommunity.com/id/#{steam_name}/?xml=1").parsed_response
       if response['profile'].present? 
         self.steam_id = response['profile']['steamID64']
+        self.avatar = response['profile']['avatarFull']
       else
         false
       end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823124948) do
+ActiveRecord::Schema.define(version: 20140902171720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140823124948) do
     t.string   "steam_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar"
   end
 
   add_index "users", ["steam_name"], name: "index_users_on_steam_name", unique: true, using: :btree
